55ffde5e9d0988224a01a76bef7e93c1c1725a8c
jonss0777 <jonss0777@gmail.com>
2024-08-11 22:51:27 -0400

    adding gemini

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,20 +1,33 @@
+1 of 41
+s
+Inbox
+Jonathan Suconota
+	
+ 
+	10:44 PM (6 minutes ago)
+Jonathan Suconota <jsuconota1@gmail.com>
+	
+10:44 PM (6 minutes ago)
+	
+to me
 import {NextResponse} from 'next/server' // Import NextResponse from Next.js for handling responses
-import OpenAI from 'openai' // Import OpenAI library for interacting with the OpenAI API
+import {GoogleGenerativeAI} from '@google/generative-ai' // Import OpenAI library for interacting with the OpenAI API
 
 // System prompt for the AI, providing guidelines on how to respond to users
 const systemPrompt = 'You will help solve math problems' // Use your own system prompt here
 
 // POST function to handle incoming requests
 export async function POST(req) {
-  const openai = new OpenAI() // Create a new instance of the OpenAI client
-  const data = await req.json() // Parse the JSON body of the incoming request
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+  console.log("sucess connecting to Gemini");
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-  // Create a chat completion request to the OpenAI API
-  const completion = await openai.chat.completions.create({
-    messages: [{role: 'system', content: systemPrompt}, ...data], // Include the system prompt and user messages
-    model: 'gpt-4o', // Specify the model to use
-    stream: true, // Enable streaming responses
-  })
+const prompt = "Write a story about a magic backpack.";
+
+const result = await model.generateContentStream(prompt);
+
+// Print text as it comes in.
+
 
   // Create a ReadableStream to handle the streaming response
   const stream = new ReadableStream({
@@ -22,7 +35,7 @@ export async function POST(req) {
       const encoder = new TextEncoder() // Create a TextEncoder to convert strings to Uint8Array
       try {
         // Iterate over the streamed chunks of the response
-        for await (const chunk of completion) {
+        for await (const chunk of result.stream) {
           const content = chunk.choices[0]?.delta?.content // Extract the content from the chunk
           if (content) {
             const text = encoder.encode(content) // Encode the content to Uint8Array
